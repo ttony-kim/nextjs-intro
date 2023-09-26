cc422643795be81704ed2ee053a85f851cb84d5e
Dasom Kim <ektha115@gmail.com>
2023-09-26 11:07:41 +0900

    2.0 Patterns

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: white;
-        }
-      `}</style>
-    </>
+    </Layout>
   );
 }
 // /about을 호출 할때 about.js를 Component에 넣어준다.
