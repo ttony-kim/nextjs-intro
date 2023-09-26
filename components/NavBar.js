803db1b0f3145efc7cd7412f16bb902c079e755e
Dasom Kim <ektha115@gmail.com>
2023-09-26 13:59:33 +0900

    2.1 Fetching Data

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,33 +1,51 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "./NavBar.module.css";
+import Image from "next/image";
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
     <nav>
-      <Link href="/" className={router.pathname === "/" ? "active" : ""}>
-        <span className={router.pathname === "/" ? "active" : ""}>Home</span>
-      </Link>
-      <Link href="/about">
-        <span className={router.pathname === "/about" ? "active" : ""}>
-          About
-        </span>
-      </Link>
-      <style jsx>
-        {`
-          nav {
-            background-color: tomato;
-          }
-          a {
-            text-decoration: none;
-          }
-          .active {
-            color: yellow;
-          }
-        `}
-      </style>
+      <Image src="/vercel.svg" alt="hello" width={50} height={50} />
+      <div>
+        <Link href="/" className={router.pathname === "/" ? "active" : ""}>
+          <span className={router.pathname === "/" ? "active" : ""}>Home</span>
+        </Link>
+        <Link href="/about">
+          <span className={router.pathname === "/about" ? "active" : ""}>
+            About
+          </span>
+        </Link>
+      </div>
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
     </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo.js";
 
+const API_KEY = "3847f4ec54379ce41e60062f00a80bc6";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
